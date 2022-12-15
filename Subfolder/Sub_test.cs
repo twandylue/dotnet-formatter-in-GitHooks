953c62b6342e5dd65b7679d06fb8124d42970697
AndyLu <laserlue@gmail.com>
2022-12-15 21:43:39 +0800

    test: test pre-commit -14

--- Subfolder/Sub_test.cs
+++ Subfolder/Sub_test.cs
@@ -1,7 +1,7 @@
 ﻿namespace dotnet_formatter_in_GitHooks.Subfolder;
 
-public class Sub_test     {
-    public string? Name_123    {
-        get; set   ;
+public class Sub_test {
+    public string? Name_123 {
+        get; set;
     }
 }
