5007aba2327f3aee45d6c9d95ade8e3ea3fe7e7b
AndyLu <laserlue@gmail.com>
2022-12-16 10:35:11 +0800

    demo: init

--- Subfolder/Sub_test.cs
+++ Subfolder/Sub_test.cs
@@ -1,7 +1,8 @@
 ﻿namespace dotnet_formatter_in_GitHooks.Subfolder;
 
-public class Sub_test {
-    public string? Name_123 {
+public
+    class Sub_test {
+    public string? Name {
         get; set;
     }
 }
